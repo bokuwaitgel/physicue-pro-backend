8e1faaead9bbf6fe521a32cc9090e289fe1ecf2c
Itgel Rokuro <itgl@Itgels-MacBook-Air.local>
2025-06-15 14:18:29 +0800

    update

--- src/cources/exercise/exercise.controller.ts
+++ src/cources/exercise/exercise.controller.ts
@@ -75,6 +75,8 @@ export class ExerciseController {
           return decoded;
       }
       const userId = decoded.data.id;
+      
+    console.log('checkExercise', data, userId);
     
     return this.exerciseService.checkExercise(data.exerciseId, data.courseId, userId);
   }
