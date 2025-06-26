79108e835f995956e9343241e995f496846787d4
Itgel <>
2025-06-26 11:39:02 +0800

    create course description update

--- src/cources/course/course.dto.ts
+++ src/cources/course/course.dto.ts
@@ -15,6 +15,8 @@ export class CreateCourseDto {
   @ApiProperty()
   @IsNotEmpty()
   price: string;
+  @ApiProperty()
+  description: string;
 }
 
 
--- src/cources/course/course.service.ts
+++ src/cources/course/course.service.ts
@@ -40,6 +40,7 @@ export class CourseService {
           title: data.title,
           duration: parseInt(data.duration),
           price: parseFloat(data.price),
+          description: data.description,
           type: types,
           teacher: {
             connect: {
