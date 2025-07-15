b1bb468dda0821463259cb62007278f021c89df3
Itgel Rokuro <itgl@Itgels-MacBook-Air.local>
2025-07-16 02:17:40 +0800

    post

--- src/groups/post/post.module.ts
+++ src/groups/post/post.module.ts
@@ -6,9 +6,10 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+import { AwsS3Service } from 'src/s3.service';
 
 @Module({
-  providers: [PostService, AuthService, PrismaService, JwtAuthGuard, JwtService, UsersService],
+  providers: [PostService, AuthService, PrismaService, JwtAuthGuard, JwtService, UsersService, AwsS3Service],
   controllers: [PostController],
   exports: [PostService],
 })
